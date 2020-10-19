911e62c4f0e98c89364812f97e403e53bd417bfd
Dmitry Izumskiy <Dmitry_Izumskiy@intuit.com>
2020-10-19 09:41:35 -0700

    Initial Commit

---
+++ Day1Tests/Day1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Day1Tests.swift
+//  Day1Tests
+//
+//  Created by Izumskiy, Dmitry on 10/19/20.
+//
+
+import XCTest
+@testable import Day1
+
+class Day1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
